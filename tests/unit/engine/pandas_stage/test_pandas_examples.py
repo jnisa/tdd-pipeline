7a02f945d071a87269eef6f7bc52ba6c9ed3c3e3
Nisa <joao.nisa@digitalhub.man>
2021-10-20 14:29:25 +0100

    add the test part to the pandas functions created on the last commit

--- tests/unit/engine/pandas_stage/test_pandas_examples.py
+++ tests/unit/engine/pandas_stage/test_pandas_examples.py
@@ -2,6 +2,7 @@
 
 
 import unittest
+import pandas as pd
 
 
 
@@ -18,17 +19,51 @@ class DropNanRecordsTestScenarios(unittest.TestCase):
         Complexity - 1/4
         '''
 
-        return None
+        data  = {
+            'Name': ['Tay', 'Bonito', 'Marques', 'Ricardo'],
+            'Age': [23, None, 19, 50],
+            'Position': ['Agile Enabler', 'Junior Data Engineer', 'DevOps', 'Data Engineer']
+        }
+
+        expected = {
+            'Name': ['Tay', 'Marques', 'Ricardo'],
+            'Age': [23, 19, 50],
+            'Position': ['Agile Enabler', 'DevOps', 'Data Engineer']
+        }
+
+
+        df = pd.DataFrame(data)
+        df_result = drop_nan_records(df)
+        df_expected = pd.DataFrame(expected)
+
+        return self.assertEqual(df_result, df_expected)
 
     
     def test_drop_nan_records_tc2(self):
 
         '''
         drop_nan_records - 2nd Test Scenario
-        Complexity - 1/4
+        Complexity - 2/4
         '''
 
-        return None
+        data = {
+            'Name': ['Tay', 'Bonito', 'Marques', None],
+            'Age': [23, None, 19, 50],
+            'Position': ['Agile Enabler', 'Junior Data Engineer', None, 'Data Engineer']
+        }
+
+        expected = {
+            'Name': ['Tay'],
+            'Age': [23],
+            'Position': ['Agile Enabler']
+        }
+
+
+        df = pd.DataFrame(data)
+        df_result = pd.DataFrame(df)
+        df_expected = pd.DataFrame(expected)
+
+        return self.assertEqual(df_result, df_expected)
 
 
 
@@ -41,14 +76,58 @@ class SchemaApprovalTestScenarios(unittest.TestCase):
         Complexity - 1/4
         '''
 
-        return None
+        data = {
+            'Name': ['Tay', 'Bonito', 'Marques', 'Ricardo'],
+            'Age': [23, '18', 19, 50],
+            'Position': ['Agile Enabler', 'Junior Data Engineer', 'DevOps', 'Data Engineer']
+        }
+
+        expected = {
+            'Name': ['Tay', 'Marques', 'Ricardo'],
+            'Age': [23, 19, 50],
+            'Position': ['Agile Enabler', 'DevOps', 'Data Engineer']
+        }
+
+        schema = {
+            'Name': str,
+            'Age': int,
+            'Position': str
+        }
+
+        df = pd.DataFrame(data)
+        df_result = schema_approval(df, schema)
+        df_expected = pd.DataFrame(expected)
+
+        return self.assertEqual(df_result, df_expected)
 
 
     def test_schema_approval_tc2(self):
 
         '''
         schema_approval: 2nd Test Scenario
-        Complexity - 1/4
+        Complexity - 2/4
         '''
-        
-        return None
+
+        data = {
+            'Name': [1, 'Bonito', 'Marques', 'Ricardo'],
+            'Age': [23, '18', 19, 50],
+            'Position': ['Agile Enabler', 'Junior Data Engineer', 1, 'Data Engineer']
+        }
+
+        expected = {
+            'Name': ['Ricardo'],
+            'Age': [50],
+            'Position': ['Data Engineer']
+        }
+
+        schema = {
+            'Name': str,
+            'Age': int,
+            'Position': str
+        }
+
+        df = pd.DataFrame(data)
+        df_result = schema_approval(df, schema)
+        df_expected = pd.DataFrame(expected)
+
+        return self.assertEqual(df_result, df_expected)
