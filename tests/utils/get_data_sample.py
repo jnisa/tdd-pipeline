e9d52fc708b03a8dbb29339f458f537838d95a90
Nisa <joao.nisa@digitalhub.man>
2021-12-03 15:20:23 +0000

    querying athena working

--- tests/utils/get_data_sample.py
+++ tests/utils/get_data_sample.py
@@ -1,25 +1,28 @@
 
 
-'''
-Commands that work from the aws cli:
-. aws athena start-query-execution --query-string "SELECT * FROM man_vehicledatalake_dev_gluedatabase_rio_landing.man_servicecare_workshops_country LIMIT 10" --result-configuration "OutputLocation=s3://aws-athena-query-results-854474629353-eu-west-1/"
-. aws athena get-query-results --query-execution-id "dd5c38cb-f6a3-4165-a96f-33881e9f1fcb"
-'''
-
 
 import pdb
-import os
-from os import listdir
-from os.path import isfile, join
 
+import ast
+import json
+import time
 import subprocess
+from os import walk, curdir
+from os.path import abspath
 
 
 class getDataSample:
 
-    def __init__(self, path):
+    def __init__(self, path, output_bucket):
 
         self.path = path
+        self.output_bucket = output_bucket
+
+        self.listTestCases()
+        self.listQueries()
+        self.queryAWSData()
+        self.getDataFromQuery()
+        self.writeDataSamples()
 
     
     def listTestCases(self):
@@ -28,7 +31,7 @@ class getDataSample:
         list all the test cases that comprehend the queries to get the data samples
         '''
 
-        self.files_lst = ['/'.join([f[0], f[2][0]]) for f in os.walk(self.path) if 'query_' in '/'.join([f[0], f[2][0]])]
+        self.files_lst = ['/'.join([f[0], f[2][0]]) for f in walk(self.path) if 'query_' in '/'.join([f[0], f[2][0]])]
 
         return self
 
@@ -44,8 +47,6 @@ class getDataSample:
         for f in self.files_lst:
             self.query_lst.append(open(f, "r").read().replace("\n", " "))
 
-        pdb.set_trace()
-
         return self
 
 
@@ -55,7 +56,15 @@ class getDataSample:
         creates a file with the data samples retrieved from the queries defined
         '''
 
-        return None
+        dirs_lst = ['/'.join(f.split('/')[:-1]) for f in self.files_lst]
+
+        for dir in dirs_lst:
+
+            with open('/'.join([dir, 'data_sample.txt']), 'w') as convert_file:
+
+                convert_file.write(json.dumps(list(self.data_store.values())[dirs_lst.index(dir)].decode("utf-8")))
+
+        return self
 
     
     def queryAWSData(self):
@@ -64,23 +73,54 @@ class getDataSample:
         query the database and get the query execution id
         '''
 
-        return None
+        self.execution_map = {}
+
+        for q in self.query_lst:
+
+            query_obs = ["aws", "athena", "start-query-execution", 
+                "--query-string", "'%s'" %(q), 
+                "--result-configuration", "'OutputLocation=%s'" %(self.output_bucket)]
+            
+            query_execution = subprocess.Popen(" ".join(query_obs), shell=True, stdout=subprocess.PIPE)
+            self.execution_map[q] = ast.literal_eval(query_execution.stdout.read().decode('utf-8'))['QueryExecutionId']
+
+        return self
 
 
     def getDataFromQuery(self):
 
         '''
         get the query results into a spark dataframe
+        
+        Obs: some stopping times were added to adjust the buffer to the query performed
         '''
 
-        return None
+        self.data_store = {}
+
+        for pid in list(self.execution_map.values()):
+
+            time.sleep(0.5)
+
+            data = subprocess.check_output(
+                'aws athena get-query-results --query-execution-id %s' %(pid),
+                shell=True
+            )
+            self.data_store[pid] = data
+
+            time.sleep(0.5)
+
+        return self
 
 
 
-ROOT_DIR = os.path.abspath(os.curdir)
-proj_path = ['tests', 'unit', 'engine', 'cloud_stage']
+# definition of the full path 
+ROOT_DIR = abspath(curdir)
+proj_path = ['tests', 'unit', 'engine', 'athena_stage']
+full_path = '/'.join([ROOT_DIR, '/'.join(proj_path)])
+
+# definition of the query output bucket location (configuration or text file)
+bucket_out = "s3://aws-athena-query-results-854474629353-eu-west-1/"
 
 
-ds = getDataSample('/'.join([ROOT_DIR, '/'.join(proj_path)]))
-ds.listTestCases()
-ds.listQueries()
+# call class functions
+ds = getDataSample(full_path, bucket_out)
